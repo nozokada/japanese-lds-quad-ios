630b1fe6e7711d2ee3dec1e12b63d046af22bde5
Nozomi Okada <nokada@proofpoint.com>
2020-02-03 22:20:51 -0700

    Adding ContentViewController and PagesViewController

---
+++ JapaneseLDSQuad/Controller/PagesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PagesViewController.swift
+//  JapaneseLDSQuad
+//
+//  Created by Nozomi Okada on 2/3/20.
+//  Copyright © 2020 nozokada. All rights reserved.
+//
+
+import UIKit
+
+class PagesViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
