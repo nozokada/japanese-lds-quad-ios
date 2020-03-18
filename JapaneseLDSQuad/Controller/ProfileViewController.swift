0e8599c7e88d6d3dd21f85f1270976cc48e8642c
Nozomi Okada <nokada@proofpoint.com>
2020-03-17 20:54:40 -0600

    Adding ProfileViewController and the tab bar icon

---
+++ JapaneseLDSQuad/Controller/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  JapaneseLDSQuad
+//
+//  Created by Nozomi Okada on 3/17/20.
+//  Copyright © 2020 nozokada. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
