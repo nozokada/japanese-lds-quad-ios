91755d392d45e62b4d1518b179661d9459c03f9d
Nozomi Okada <nokada@proofpoint.com>
2020-03-21 16:00:35 -0600

    Adding AuthenticationManager

---
+++ JapaneseLDSQuad/Manager/AuthenticationManager.swift
@@ -0,0 +1,15 @@
+//
+//  AuthenticationManager.swift
+//  JapaneseLDSQuad
+//
+//  Created by Nozomi Okada on 3/21/20.
+//  Copyright © 2020 nozokada. All rights reserved.
+//
+
+import Foundation
+
+class AuthenticationManager {
+    
+    static let shared = AuthenticationManager()
+    
+}
