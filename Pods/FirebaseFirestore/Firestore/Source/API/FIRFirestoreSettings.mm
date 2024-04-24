6fa61855d5afac9d482e2587c7ce262d3193ec58
Nozomi Okada <nozokada@gmail.com>
2024-04-23 19:35:14 -0600

    Reinstall Pods after upgrading to Xcode 15.3

--- Pods/FirebaseFirestore/Firestore/Source/API/FIRFirestoreSettings.mm
+++ Pods/FirebaseFirestore/Firestore/Source/API/FIRFirestoreSettings.mm
@@ -31,7 +31,7 @@ using api::Settings;
 using util::ThrowInvalidArgument;
 
 // Public constant
-ABSL_CONST_INIT extern "C" const int64_t kFIRFirestoreCacheSizeUnlimited =
+extern "C" const int64_t kFIRFirestoreCacheSizeUnlimited =
     api::Settings::CacheSizeUnlimited;
 
 @implementation FIRFirestoreSettings
